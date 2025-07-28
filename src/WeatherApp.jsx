2bc1c025edefe6ab3bb637318b0bde4340ec32ad
Nakul <nakulkumar160162@gmail.com>
2025-07-28 10:54:17 +0530

    change in raw data

--- src/WeatherApp.jsx
+++ src/WeatherApp.jsx
@@ -1,16 +1,17 @@
 import SearchBox from "./SearchBox";
 import InfoBox from "./InfoBox";
 import React, { useState } from 'react';
+import { Description } from "@mui/icons-material";
 
 export default function WeatherApp() {
     const [weatherInfo, setWeatherInfo] = useState({
         city: "Delhi",
-        feelslike: "24.84°C",
-        temp: "25.05°C",
+        feelslike: "24.84",
+        temp: "25.05",
         humidity: "60%",
-        tempMin: "24.00°C",
-        tempMax: "26.00°C",
-        weather: "Clouds",
+        tempMin: "24.00",
+        tempMax: "26.00",
+        description: "Clouds",
     });
 
     let updateWeatherInfo = (newInfo) => { setWeatherInfo(newInfo); };
